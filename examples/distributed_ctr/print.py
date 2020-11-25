eadd4fb48721a4c9aeb6ac19bc11d5b35747fa7f
seiriosPlus <tangwei12@baidu.com>
2020-11-25 14:35:38 +0800

    add print

---
+++ examples/distributed_ctr/print.py
@@ -0,0 +1,13 @@
+import paddle
+paddle.enable_static()
+
+from paddle.fluid.framework import Program
+
+model_basename = "__model__"
+model_filename = model_basename
+
+with open(model_filename, "rb") as f:
+    program_desc_str = f.read()
+
+program = Program.parse_from_string(program_desc_str)
+print(program)
